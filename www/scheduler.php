11670adf83520b20e8bfb070b6f1627dc146ad86
guggenbergerME <github@guggenberger.me>
2025-10-22 17:40:04 +0000

    init

--- www/scheduler.php
+++ www/scheduler.php
@@ -1,4 +1,8 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/scheduler_error.log');
 require_once 'db.php';
 date_default_timezone_set('Europe/Berlin');
 
